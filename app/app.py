879a0d86a1ed31db60564585f397081ae5ea3480
Ivana Parrilla <ip22@njit.edu>
2021-07-19 20:17:36 -0400

    Task: Added and updated app.py file, Dockerfile, and Requirements

--- app/app.py
+++ app/app.py
@@ -6,4 +6,32 @@ from flask import Flask, Response
 app = Flask(__name__)
 
 
-def cities_import() 
+def cities_import() -> List[Dict] :
+    config = {
+        'user': 'root',
+        'password': 'root',
+        'host': 'db',
+        'port': '3306'
+        'database': 'citiesData'
+    }
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor(dictionary=True)
+
+    cursor.execute('SELECT * FROM tblCitiesImport')
+    result = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    return result
+
+
+@app.route('/')
+def index() -> str:
+    js = json.dumps(cities_import())
+    resp = Response(js, status=200, mimetype='application/json')
+    return resp
+
+
+if__name__== '__main__':
+    app.run(host='0.0.0.0')
